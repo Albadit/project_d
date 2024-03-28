8aecd171930d2a040aeb98e31c7ff11f1e8550c9
spronu <80100046+spronu@users.noreply.github.com>
2024-03-28 14:24:07 +0100

    Ardit zijn code mergen met die van Yahya (#1)
    * update: add fix bug
    
    * clean up
    
    * Update README.md
    
    * Update analyse.py
    
    ---------
    
    Co-authored-by: Albadit <46565849+Albadit@users.noreply.github.com>

--- analyse.py
+++ analyse.py
@@ -2,17 +2,18 @@ import os
 import json
 from langdetect import detect
 
-def read_json(file_name: str) -> dict:
+# type export: dict or list
+def read_json(file_name: str): 
   with open(file_name, 'r') as file:
     data = json.load(file)
 
   return data
 
-def write_json(data, file_name: str):
+def write_json(data, file_name: str) -> None:
   with open(os.path.join(os.getcwd(), file_name), 'w') as file:
     json.dump(data, file, indent=2)
 
-def check_null(data: dict) -> dict:
+def check_null(data: dict) -> None:
   check = {key: False for key in data[0]}
 
   for row in data:
@@ -20,16 +21,21 @@ def check_null(data: dict) -> dict:
       if check[key] == False:
         if value:
           check[key] = True
+  
+  print(check)
 
-def check_total(data: dict) -> dict:
+def check_total(data: dict) -> None:
   check = {key: 0 for key in data[0]}
 
   for row in data:
     for key, value in row.items():
       if value:
         check[key] += 1
+  
+  print(check)
+  print(len(check))
 
-def merge_table(actionscopes: dict, service_joborders: dict) -> dict:
+def merge_table(actionscopes: dict, service_joborders: dict) -> list:
   table = []
   for i in actionscopes:
     merge = {}
@@ -39,44 +45,45 @@ def merge_table(actionscopes: dict, service_joborders: dict) -> dict:
         merge['Service_joborders'] = j
         table.append(merge)
         break
-    # else: 
-    #   merge = i
-    #   merge['Service_joborders'] = None
-    #   table.append(merge)
     else:
       table.append(i)
 
   return table
 
-def info_table(merge_table: dict) -> dict:
+def problem_table(merge_table: dict) -> list:
   table = []
-  for index, i in enumerate(merge_table):
+  for i in merge_table:
     row = {
-      'Actionscope.Code': i['Code'],
+      'actionscope_code': i['Code'],
       'actionScopes_serviceDescription': i['Service object.Description'],
       'actionScopes_description': i['Description'],
       'actionScopes_memo': i['Memo'],
       'actionScopes_servicetype': i['Servicetype'],
     }
     
-    # if i['Joborders'] and i['Service_joborders']:
-    if i['Joborders']:
-      print(index)
-      print(i['Code'])
-      row['jobOrders_serviceDescription'] = i['Service_joborders']['Service object.Description']
-      row['jobOrders_description'] = i['Service_joborders']['Description']
-      row['jobOrders_memo'] = i['Service_joborders']['Memo']
-      row['jobOrders_internalMemo'] = i['Service_joborders']['Internal memo']
-      row['jobOrders_problem'] = i['Service_joborders']['Problem (not visible on tablet)']
-      row['jobOrders_realizedSolution'] = i['Service_joborders']['Realized solution']
-      row['jobOrders_servicetype'] = i['Service_joborders']['Service type']
+    if i['Joborders'] and 'Service_joborders' in i:
+      joborders = {
+        'jobOrders_serviceDescription': i['Service_joborders']['Service object.Description'],
+        'jobOrders_description': i['Service_joborders']['Description'],
+        'jobOrders_memo': i['Service_joborders']['Memo'],
+        'jobOrders_internalMemo': i['Service_joborders']['Internal memo'],
+        'jobOrders_problem': i['Service_joborders']['Problem (not visible on tablet)'],
+        'jobOrders_realizedSolution': i['Service_joborders']['Realized solution'],
+        'jobOrders_servicetype': i['Service_joborders']['Service type']
+      }
+      row.update(joborders)
 
     table.append(row)
 
   return table
 
-def check_joborders(merge_table: dict) -> dict:
-  pass
+def check_joborders(merge_table: dict) -> list:
+  table = []
+  for i in merge_table:
+    if i['Joborders'] and not 'Service_joborders' in i:
+      table.append(i['Code'])
+
+  print(table)
 
 
 if __name__ == '__main__':
@@ -88,18 +95,16 @@ if __name__ == '__main__':
   actionscopes = read_json(file2)
   mergeTable = read_json(file3)
 
-  # null = check_null(service_joborders)
-  # total = check_total(service_joborders)
-  # print(null)
-  # print(total, len(total))
-
-  # merge = merge_table(actionscopes, service_joborders)
-  # write_json(merge, 'analyse\\merge_table.json')
-
   # print(detect("Pos.010 PC11-IV. Automatic pricking robot equipped with 4 grippers"))
   # print(detect("Onderhoud mechanica met inspectie Basis pakket 2017/2018"))
   # print(detect("SPS Ausgang ist kaput und m\u00fcst umprogramiert werden.\n"))
 
-  info = info_table(mergeTable)
-  # write_json(info, 'analyse\\info.json')
+  # check_null(service_joborders)
+  # check_total(service_joborders)
+  check_joborders(mergeTable)
 
+  # merge = merge_table(actionscopes, service_joborders)
+  # write_json(merge, 'analyse\\merge_table.json')
+
+  # info_problem = problem_table(mergeTable)
+  # write_json(info_problem, 'analyse\\problem_table.json')
